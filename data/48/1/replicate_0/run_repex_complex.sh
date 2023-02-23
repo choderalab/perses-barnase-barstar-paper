fe0c2053cab9fa97e561f5c813f6ced580e8e143
zhang-ivy <ivy.zhang@wustl.edu>
2023-02-23 11:20:50 -0500

    add main dir 48

---
+++ data/48/1/replicate_0/run_repex_complex.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 36:00
+#
+# Set output file
+#BSUB -o run_repex_complex.out
+#
+# Set error file
+#BSUB -eo run_repex_complex.stderr
+#
+# Specify node group
+#BSUB -m "lj-gpu ll-gpu ln-gpu ly-gpu lx-gpu lu-gpu ld-gpu"
+#BSUB -q gpuqueue
+#
+# nodes: number of nodes and GPU request
+#BSUB -R "rusage[mem=3]"
+#BSUB -n 2 -gpu "num=1/task:j_exclusive=yes:mode=shared"
+#
+# job name (default = name of script file)
+#BSUB -J "48.1.com"
+
+source ~/.bashrc
+conda activate perses-paper5
+
+outdir=/data/chodera/zhangi/perses_benchmark/repex/perses-bnbs-paper-fourth-attempt/48/1/replicate_0/
+phase=complex
+n_states=36
+n_cycles=10000
+t_max=300
+restraint='heavy'
+force_constant=50.0
+
+build_mpirun_configfile --configfilepath configfile_${phase} --hostfilepath hostfile_${phase} "python /data/chodera/zhangi/perses_benchmark/perses-barnase-barstar-paper/scripts/04_run_repex/run_repex.py $outdir $phase $n_states $n_cycles $t_max --restraint $restraint --force_constant $force_constant"
+mpiexec.hydra -f hostfile_${phase} -configfile configfile_${phase}
