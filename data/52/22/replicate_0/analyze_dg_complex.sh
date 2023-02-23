e4d8de29867a308c7ab65d68188879a0b4ee522b
zhang-ivy <ivy.zhang@wustl.edu>
2023-02-23 11:21:25 -0500

    add main dir 52

---
+++ data/52/22/replicate_0/analyze_dg_complex.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 1:00 
+#
+# Set output file
+#BSUB -o analyze_dg_complex.out
+#
+# Set error file
+#BSUB -eo analyze_dg_complex.stderr
+#
+# Specify node group
+#BSUB -q cpuqueue
+#
+# nodes: number of nodes and GPU request
+#BSUB -R "rusage[mem=3]"
+#BSUB -n 24
+#
+# job name (default = name of script file)
+#BSUB -J "52.22a"
+
+source ~/.bashrc
+conda activate perses-paper5
+
+main_dir=52
+sub_dir=22
+replicate=0
+phase=complex
+total_iterations=50000
+
+python /data/chodera/zhangi/perses_benchmark/perses-barnase-barstar-paper/scripts/05_analyze/analyze_dg.py $main_dir $sub_dir $replicate $phase $total_iterations --bootstrap
+
---
+++ data/52/27/replicate_0/generate_modified_htfs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Set walltime limit
+#BSUB -W 2:00 
+#
+# Set output file
+#BSUB -o  generate_modified_htfs.out
+#
+# Set error file
+#BSUB -eo generate_modified_htfs.stderr
+#
+# Specify node group
+#BSUB -m "ly-gpu lx-gpu lu-gpu lt-gpu"
+#BSUB -q gpuqueue
+#
+# nodes: number of nodes and GPU request
+#BSUB -n 1 -R "rusage[mem=3]" -sp 25
+#BSUB -gpu "num=1:j_exclusive=yes:mode=shared"
+#
+# job name (default = name of script file)
+#BSUB -J "htf.27"
+
+in_dir=/data/chodera/zhangi/perses_benchmark/repex/perses-bnbs-paper-fourth-attempt/47/27/replicate_0/
+out_dir=/data/chodera/zhangi/perses_benchmark/repex/perses-bnbs-paper-fourth-attempt/52/27/replicate_0/
+
+source ~/.bashrc
+conda activate perses-paper5
+
+cd /data/chodera/zhangi/perses_benchmark/perses-barnase-barstar-paper/scripts/03_generate_htfs/
+python generate_modified_htfs.py $in_dir $out_dir --rest_radius 0.5
